da6e20f7879d07d266c2f3406ba5d128af5ef09f
Matt Pilott <hello@matt-pilott.com>
2024-10-07 13:59:22 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { composeVisitors } from 'lightningcss'
 import { formatDate } from 'kitto'
 import { breakpoints, fluid, size } from 'kitto/lightningcss'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { avatarCount } from './src/library/avatar_count.js'
 
 const { name, version } = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'))
 
@@ -16,12 +17,11 @@ export default defineConfig({
 			drafts: { customMedia: true },
 			visitor: composeVisitors([
 				breakpoints({
-					mobile: 640,
-					tablet: 1024,
-					laptop: 1280,
-					desktop: 1440
+					small: 320,
+					medium: 360,
+					large: 390
 				}),
-				fluid({ vmax: 1600 }),
+				fluid({ vmax: 360 }),
 				size
 			])
 		}
@@ -31,7 +31,7 @@ export default defineConfig({
 		'import.meta.env.version': JSON.stringify(version),
 		'import.meta.env.build': JSON.stringify(formatDate('{DD}-{MM}-{YYYY}@{HH}:{mm}:{ss}'))
 	},
-	plugins: [sveltekit(), basicSsl()],
+	plugins: [sveltekit(), basicSsl(), avatarCount()],
 	// resolve: { extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svelte'] },
 	server: { proxy: {} }
 })
