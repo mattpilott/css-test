769cc3c19dbd19c24281756fe62aaf2d3ffa348c
Matt Pilott <hello@matt-pilott.com>
2024-10-07 14:01:44 +0100

    strip styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,20 +7,3 @@
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 	<Button href="hello">Hello</Button>
 </div>
-
-<style lang="css">
-	.foo {
-		color: blue;
-
-		.bar {
-			color: red;
-		}
-	}
-	h1 {
-		font: 600 var(--f-h1);
-
-		@media (--from-mobile) and (--until-tablet) {
-			color: red;
-		}
-	}
-</style>
